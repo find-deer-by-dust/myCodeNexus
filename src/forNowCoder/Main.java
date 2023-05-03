10826c44d33f01d3a225f86d51218c2d9197e401
find-deer-by-dust <1915141975@qq.com>
2023-05-03 22:01:46 +0800

    test

---
+++ src/forNowCoder/Main.java
@@ -0,0 +1,9 @@
+package forNowCoder;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("hello world");
+	}
+
+}
---
+++ src/forNowCoder/Solution.java
@@ -0,0 +1,11 @@
+package forNowCoder;
+
+import java.util.ArrayList;
+
+public class Solution {
+    public ArrayList<String> Permutation(String str) {
+        ArrayList<String> re = new ArrayList<String>();
+
+        return re;
+    }
+}
