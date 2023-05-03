629f76be36c212443f0b1cd28f70dd7f589c8010
find-deer-by-dust <1915141975@qq.com>
2023-05-03 21:13:34 +0800

    nothing

---
+++ 416.分割等和子集.java
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode.cn id=416 lang=java
+ *
+ * [416] 分割等和子集
+ */
+
+// @lc code=start
+class Solution {
+    public boolean canPartition(int[] nums) {
+
+        return false;
+    }
+
+}
+// @lc code=end
