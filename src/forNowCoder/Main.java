68240aeba4fcccad32389169084cccfe880bdd9d
find-deer-by-dust <1915141975@qq.com>
2023-05-04 15:09:35 +0800

    jz38

--- src/forNowCoder/Main.java
+++ src/forNowCoder/Main.java
@@ -1,9 +1,16 @@
 package forNowCoder;
 
+import java.util.ArrayList;
+
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("hello world");
+		Solution s = new Solution();
+		ArrayList<String> x = s.Permutation("qwertyuio");
+
+		// for (String words : x) {
+		// System.out.println(words);
+		// }
 	}
 
 }
--- src/forNowCoder/Solution.java
+++ src/forNowCoder/Solution.java
@@ -1,11 +1,42 @@
 package forNowCoder;
 
-import java.util.ArrayList;
+import java.util.*;
 
 public class Solution {
     public ArrayList<String> Permutation(String str) {
-        ArrayList<String> re = new ArrayList<String>();
+        ArrayList<String> res = new ArrayList<>();
+        char[] chars = str.toCharArray();
+        Permutation(chars, res, 0);
 
-        return re;
+        return res;
+    }
+
+    public void Permutation(char[] chars, ArrayList<String> res, int i) {
+        if (i == chars.length) {
+            res.add(new String(chars));
+            return;
+        }
+        // 每一层都使用一个hashset 来保存没有重复的字符，如果有重复，则不递归尝试获取排列
+        HashSet<Character> temp = new HashSet<>();
+        // 每个j位置的数都有机会来到i位置
+        for (int j = i; j < chars.length; j++) {
+            // 只要set中不包含j索引的字符，那么递归求解
+            if (!temp.contains(chars[j])) {
+                // 添加记录
+                temp.add(chars[j]);
+                // 交换位置
+                permutationSwap(chars, i, j);
+                // 递归求解
+                Permutation(chars, res, i + 1);
+                // 交换回来，下次循环继续
+                permutationSwap(chars, i, j);
+            }
+        }
+    }
+
+    public void permutationSwap(char[] chars, int i, int j) {
+        char temp = chars[i];
+        chars[i] = chars[j];
+        chars[j] = temp;
     }
 }
