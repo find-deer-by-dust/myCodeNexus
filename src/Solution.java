4a88911be4cd59a58d7cf913310969675c75f47d
find-deer-by-dust <1915141975@qq.com>
2023-05-05 17:59:33 +0800

    for nothing

--- src/Solution.java
+++ src/Solution.java
@@ -1,5 +1,12 @@
 import java.util.ArrayList;
 
-public class Solution {
+class Solution {
+    public int findCircleNum(int[][] isConnected) {
+        int num=isConnected.length;
+        int re =0;
+        int [] used = new int[num];
 
-}
+
+        return re;
+    }
+}​ ​
