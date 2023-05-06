7e885424646d86afe800b7284b92c6439bdf6011
find-deer-by-dust <1915141975@qq.com>
2023-05-06 14:46:22 +0800

    for nothing

--- src/Solution.java
+++ src/Solution.java
@@ -1,12 +1,6 @@
-import java.util.ArrayList;
+public class Solution {
+    public int FindGreatestSumOfSubArray(int[] array) {
 
-class Solution {
-    public int findCircleNum(int[][] isConnected) {
-        int num=isConnected.length;
-        int re =0;
-        int [] used = new int[num];
-
-
-        return re;
+        return 1;
     }
-}​ ​
+}
