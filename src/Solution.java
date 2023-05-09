e5772f7723ef30ce98281fc325f8ef4058d0c530
find-deer-by-dust <1915141975@qq.com>
2023-05-09 18:12:59 +0800

    add test.dio

--- src/Solution.java
+++ src/Solution.java
@@ -1,6 +1,19 @@
 public class Solution {
     public int FindGreatestSumOfSubArray(int[] array) {
-
+        if (array == null)
+            return 0;
+        if (array.length == 0)
+            return 0;
+        int min = 0;
+        int minNum = 0;
+        int tmp = 0;
+        for (int i = 0; i < array.length; i++) {
+            tmp = tmp + array[i];
+            if (tmp < min) {
+                min = tmp;
+                minNum = i;
+            }
+        }
         return 1;
     }
 }
