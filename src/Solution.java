5ecdc84e79e378802b902daba5e0228a6953de0c
find-deer-by-dust <1915141975@qq.com>
2023-05-09 23:33:55 +0800

    nothing

--- src/Solution.java
+++ src/Solution.java
@@ -5,15 +5,28 @@ public class Solution {
         if (array.length == 0)
             return 0;
         int min = 0;
-        int minNum = 0;
+        int minNum1 = -1;
+        int minNum2 = array.length;
         int tmp = 0;
         for (int i = 0; i < array.length; i++) {
             tmp = tmp + array[i];
             if (tmp < min) {
                 min = tmp;
-                minNum = i;
+                minNum1 = i;
             }
         }
-        return 1;
+        min = 0;
+        tmp = 0;
+        for (int i = array.length - 1; i >= 0; i--) {
+            tmp = tmp + array[i];
+            if (tmp < min) {
+                min = tmp;
+                minNum2 = i;
+            }
+        }
+        int re = 0;
+        for (int i = minNum1 + 1; i < minNum2; i++)
+            re = re + array[i];
+        return re;
     }
 }
