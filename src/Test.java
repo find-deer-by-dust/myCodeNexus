9f758d4b363216f77efea2bcc69cfb76be64236e
find-deer-by-dust <1915141975@qq.com>
2023-05-12 21:55:56 +0800

    nothing

--- src/Test.java
+++ src/Test.java
@@ -1,41 +1,26 @@
-
 import java.util.*;
 
 public class Test {
-    public ArrayList<String> Permutation(String str) {
-        ArrayList<String> res = new ArrayList<>();
-        char[] chars = str.toCharArray();
-        Permutation(chars, res, 0);
-
-        return res;
-    }
-
-    public void Permutation(char[] chars, ArrayList<String> res, int i) {
-        if (i == chars.length) {
-            res.add(new String(chars));
-            return;
+    public int findNthDigit(int n) {
+        // 记录n是几位数
+        int digit = 1;
+        // 记录当前位数区间的起始数字：1,10,100...
+        long start = 1;
+        // 记录当前区间之前总共有多少位数字
+        long sum = 9;
+        // 将n定位在某个位数的区间中
+        while (n > sum) {
+            n -= sum;
+            start *= 10;
+            digit++;
+            // 该区间的总共位数
+            sum = 9 * start * digit;
         }
-        // 每一层都使用一个hashset 来保存没有重复的字符，如果有重复，则不递归尝试获取排列
-        HashSet<Character> temp = new HashSet<>();
-        // 每个j位置的数都有机会来到i位置
-        for (int j = i; j < chars.length; j++) {
-            // 只要set中不包含j索引的字符，那么递归求解
-            if (!temp.contains(chars[j])) {
-                // 添加记录
-                temp.add(chars[j]);
-                // 交换位置
-                permutationSwap(chars, i, j);
-                // 递归求解
-                Permutation(chars, res, i + 1);
-                // 交换回来，下次循环继续
-                permutationSwap(chars, i, j);
-            }
-        }
-    }
-
-    public void permutationSwap(char[] chars, int i, int j) {
-        char temp = chars[i];
-        chars[i] = chars[j];
-        chars[j] = temp;
+        // 定位n在哪个数字上
+        String num = "" + (start + (n - 1) / digit);
+        System.out.println(num);
+        // 定位n在数字的哪一位上
+        int index = (n - 1) % digit;
+        return (int) (num.charAt(index)) - (int) ('0');
     }
 }
