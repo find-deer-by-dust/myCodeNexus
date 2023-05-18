2761965d421a278c17e98bf72bd8e2333596da59
find-deer-by-dust <1915141975@qq.com>
2023-05-18 22:13:36 +0800

    nothing

--- py/test.py
+++ py/test.py
@@ -1,6 +1,30 @@
-from  ChatGPT import *
+import time
+import os
 
-chat=ChatGPT()
 
-botSay=chat.reply("我一个朋友这样回复我'好好好好好好,谢谢哥哥',你如何评价")
-print(botSay)
+def getFiles():
+    os.system(r'e: && cd E:\shareWithPhone &&dir > E:\shareWithPhone/2.txt')
+    with open(r"E:\shareWithPhone/2.txt", "r+", encoding='ansi') as f:
+        tmp = f.read()
+        f.close()
+
+    tmp = tmp.split('\n')
+
+    tmp = tmp[6:-3]
+    files = []
+    for i in tmp:
+        files.append(i[36:])
+    return files
+
+
+path = r"E:\shareWithPhone"
+while (True):
+    fs = getFiles()
+    p = os.system(r'ssh -p 15328 root@165.3.2.57 "ls /share " > 1.txt')
+    print(p)
+    print(fs)
+    # os.system(r"scp -P 15328 -r E:\shareWithPhone/* root@165.3.2.57:/share")
+    print(time.strftime('%H:%M', time.localtime()))
+    time.sleep(10)
+
+# scp -P 15328 -r  root@165.3.2.57:/share/* .
