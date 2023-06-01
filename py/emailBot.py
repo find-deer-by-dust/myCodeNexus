276cd8281736cf5cfe452067401119879f212a99
find-deer-by-dust <1915141975@qq.com>
2023-06-01 15:37:10 +0800

    nothing

--- py/emailBot.py
+++ py/emailBot.py
@@ -10,6 +10,7 @@ payload = "token=7XzYphnEf5YENgaq"
 headers = {'Content-Type': "application/x-www-form-urlencoded"}
 users=["3350325473@qq.com","1915141975@qq.com","2609446429@qq.com"]
 
+yesterday=''
 while(True):
     
     print(time.strftime('%H:%M', time.localtime()))
@@ -23,10 +24,15 @@ while(True):
         botSay=chat.reply('这是今天的新闻,说说你的看法,你的看法需要有政策性，针对性和准确性；在有限的篇幅中，主要靠独特的见解吸引读者；立意新颖，论述精当，文采斐然；主要面向广大群众\n'+content_text)
         
         subject="每日60秒新闻 "+tmp['data']['date']
-        for user in users:
-            print("have sent email to " + user)
-            separator='\n------我是分割线------\n'
-            server.send_mail(user, {'subject': subject, 'content_text': "ChatGPT的看法:\n"+botSay+separator+"\n原新闻:\n"+content_text})
-        
+        if yesterday!=tmp['data']['date']:
+            yesterday=tmp['data']['date']
+            for user in users:
+                print("have sent email to " + user)
+                separator='\n------我是分割线------\n'
+                server.send_mail(user, {'subject': subject, 'content_text': "ChatGPT的看法:\n"+botSay+separator+"\n原新闻:\n"+content_text})
+        else:
+            time.sleep(60*10)
+            
         time.sleep(120)
+
     time.sleep(30)
