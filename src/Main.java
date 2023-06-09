cf493a3c1f4272ddffffc6750cb803f723faec2b
lcyXG <luchengyi@xigua.city>
2023-06-09 17:15:31 +0800

    test

--- src/Main.java
+++ src/Main.java
@@ -5,10 +5,7 @@ import java.util.Comparator;
 
 public class Main {
 	public static void main(String[] args) {
-		Solution s = new Solution();
-		Test t = new Test();
-		int[] a = { 2, 5, 20 };
-		// System.out.println(s.GetUglyNumber_Solution(8));
+		System.out.println("hello world");
 	}
 
 }
