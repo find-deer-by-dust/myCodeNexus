bb157c7cdf568b4ee667d7cdf33f674fb9843a23
lcyXG <luchengyi@xigua.city>
2023-06-14 16:54:58 +0800

    比较了归并排序和冒泡

--- src/Test.java
+++ src/Test.java
@@ -1,4 +1,4 @@
-import java.util.*;
+// import java.util.*;
 
 public class Test {
     public int[] test(int[] x) {
@@ -11,19 +11,40 @@ public class Test {
         a = function1(a, 0, l);
         etime = System.currentTimeMillis();
         System.out.printf("1执行时长：%d 毫秒.\n", (etime - stime));
+        System.out.println(a[0] + "  " + a[a.length - 1]);
 
         stime = System.currentTimeMillis();
         b = function2(b);
         etime = System.currentTimeMillis();
         System.out.printf("2执行时长：%d 毫秒.\n", (etime - stime));
-
+        System.out.println(b[0] + "  " + b[b.length - 1]);
         return x;
     }
 
     int[] function1(int[] x, int a, int b) {
-        function1(x, a, b);
-
+        if (a != b) {
+            function1(x, a, (a + b) / 2);
+            function1(x, (a + b) / 2 + 1, b);
+            int i = 0;
+            while ((a + b) / 2 - i >= a) {
+                swap(x, (a + b) / 2 - i, b);
+                i++;
+            }
+        }
         return x;
+    }
+
+    void swap(int[] x, int a, int b) {
+        int tmp;
+        if (a < b) {
+            for (int i = a; i < b - 1; i++) {
+                if (x[i] > x[i + 1]) {
+                    tmp = x[i];
+                    x[i] = x[i + 1];
+                    x[i + 1] = tmp;
+                }
+            }
+        }
     }
 
     int[] function2(int[] x) {
