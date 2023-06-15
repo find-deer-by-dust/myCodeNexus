33307b25117173d7240ca1538f13619a167626d6
lcyXG <luchengyi@xigua.city>
2023-06-15 10:52:44 +0800

    add listnode

--- src/Solution.java
+++ src/Solution.java
@@ -14,7 +14,9 @@ public class Solution {
         int i;
         if (a < b - 1) {
             tmp += function1(x, a, (a + b) / 2);
+            tmp = tmp % 1000000007;
             tmp += function1(x, (a + b) / 2, b);
+            tmp = tmp % 1000000007;
             for (i = a; i < (a + b) / 2; i++) {
 
                 for (int j = (a + b) / 2; j < b; j++) {
