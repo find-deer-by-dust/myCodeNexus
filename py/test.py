4b526fe940b23fdcc30c4213604046d1d5413455
lcyXG <luchengyi@xigua.city>
2023-06-25 17:25:03 +0800

    完成了截图代码的编写，进一步优化流程

--- py/test.py
+++ py/test.py
@@ -1,10 +1,2 @@
-from selenium import webdriver
- 
-
-b = webdriver.Chrome()
-url = "https://www.baidu.com/"
-b.get(url)
- 
-
-# 关闭当前标签页（第一页）
-# b.close()
+import os
+os.system(r"cd C:\Users\Administrator\Desktop\code\for-now-coder\py\forWork\png && del * /q")
