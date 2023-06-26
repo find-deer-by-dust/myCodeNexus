4eec35602ef8995efd609165c70985cd40975412
lcyXG <luchengyi@xigua.city>
2023-06-26 16:14:01 +0800

    do nowcoder

--- src/Solution.java
+++ src/Solution.java
@@ -1,14 +1,6 @@
 import java.util.*;
 
 public class Solution {
-    /**
-     * 代码中的类名、方法名、参数名已经指定，请勿修改，直接返回方法规定的值即可
-     *
-     * 
-     * @param str string字符串
-     * @param n   int整型
-     * @return string字符串
-     */
     public String LeftRotateString(String str, int n) {
         // write code here
         int l= str.length();
