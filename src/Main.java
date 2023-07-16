2362137b03a2879ad6f5395b12429442f220ce43
find-deer-by-dust <1915141975@qq.com>
2023-07-17 01:28:11 +0800

    do nowcoder

--- src/Main.java
+++ src/Main.java
@@ -6,8 +6,8 @@ import java.util.Comparator;
 public class Main {
 	public static void main(String[] args) {
 		Solution s = new Solution();
-		int[] x = { 8, 9, 2, 5, 4, 7, 1 };
-		System.out.println(s.maxProfit(x));
+
+		System.out.println();
 	}
 
 }
--- src/Solution.java
+++ src/Solution.java
@@ -1,34 +1,8 @@
 import java.util.*;
 
 public class Solution {
-    /**
-     * 代码中的类名、方法名、参数名已经指定，请勿修改，直接返回方法规定的值即可
-     *
-     * 
-     * @param prices int整型一维数组
-     * @return int整型
-     */
-    public int maxProfit(int[] prices) {
-        // write code here
-        if (prices == null)
-            return 0;
-        if (prices.length == 0)
-            return 0;
-        int minIndex = 0;
-        int maxIndex = 0;
-        for (int i = 0; i < prices.length; i++) {
-            if (prices[i] < prices[minIndex])
-                minIndex = i;
-            if (minIndex > maxIndex)
-                maxIndex = minIndex;
-            if (prices[i] > prices[maxIndex] && i > minIndex)
-                maxIndex = i;
-        }
-        System.out.println(maxIndex);
-        System.out.println(minIndex);
+    public int Add(int num1, int num2) {
 
-        if (maxIndex > minIndex)
-            return prices[maxIndex] - prices[minIndex];
         return 0;
     }
 }
