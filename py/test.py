c0113ae4676727b06561128e7a42d6ae6497f7ea
find-deer-by-dust <1915141975@qq.com>
2023-07-19 01:44:47 +0800

    do getnews

--- py/test.py
+++ py/test.py
@@ -1,34 +1,5 @@
-
-import requests
-import json
-import xmltodict
-import nltk
 from bs4 import BeautifulSoup
-import feedparser
-import urllib.request
 
-
-url='https://36kr.com/feed-newsflash'
-
-rss = feedparser.parse(url)
-for i in rss['entries']:
-        # print(i['title'])
-        # print(i['summary'])
-        # print('--')
-
-        text=i['title']
-        soup = BeautifulSoup(i['summary'],'html.parser')
-        print(text)
-        print(soup.get_text())
-        print('---')
-
-
-
-
-# j=xml_to_json(str)
-# for i in j['rss']['channel']['item']:
-#         text=i['title']
-#         soup = BeautifulSoup(i['description'],'html.parser')
-#         print(text)
-#         print(soup.get_text())
-#         print('---')
+from tools.ChatGPT import *
+gpt=ChatGPT()
+print(gpt.reply("hi"))
--- py/tools/getNews.py
+++ py/tools/getNews.py
@@ -5,8 +5,8 @@ from ChatGPT import *
 url='https://36kr.com/feed-newsflash'
 chat=ChatGPT()
 rss = feedparser.parse(url)
-
 text=''
+
 for i in rss['entries']:
         soup = BeautifulSoup(i['summary'],'html.parser')
         textCopy=text
@@ -15,6 +15,6 @@ for i in rss['entries']:
                 text=textCopy
                 break
 
-print(text)
-botSay=chat.reply('这是今天的最新快讯,说说你的看法,你的看法需要有政策性，针对性和准确性；在有限的篇幅中，主要靠独特的见解吸引读者；立意新颖，论述精当，文采斐然；主要面向广大群众\n'+text)
+# print(text)
+botSay=chat.reply('这是今天的新闻,说说你的看法,你的看法需要有政策性，针对性和准确性；在有限的篇幅中，主要靠独特的见解吸引读者；立意新颖，论述精当，文采斐然；主要面向广大群众\n'+text)
 print(botSay)
