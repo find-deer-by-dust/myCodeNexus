5c83d1e6c5ede71189e5a6495580f449f1c5a987
lcyXG <luchengyi@xigua.city>
2023-07-19 18:17:46 +0800

    do getnews

--- py/tools/getNews.py
+++ py/tools/getNews.py
@@ -8,13 +8,20 @@ rss = feedparser.parse(url)
 text=''
 
 for i in rss['entries']:
-        soup = BeautifulSoup(i['summary'],'html.parser')
         textCopy=text
-        text=text+i['title']+'\n'+soup.get_text()+'\n\n'
+        text+=i['title']+'\n'
+
+        if '<' in i['summary']:
+                soup = BeautifulSoup(i['summary'],'html.parser')        
+                text+=soup.get_text()
+        else:
+                text+=i['summary']
+        text+='\n\n'
+
         if(len(text)>3000):
                 text=textCopy
                 break
 
-# print(text)
-botSay=chat.reply('这是今天的新闻,说说你的看法,你的看法需要有政策性，针对性和准确性；在有限的篇幅中，主要靠独特的见解吸引读者；立意新颖，论述精当，文采斐然；主要面向广大群众\n'+text)
+print(text)
+botSay=chat.reply('这是今天的新闻,你是一个专业的新闻人,你将所有新闻整合为更短的文章同时不丢失准确性和完整性\n'+text)
 print(botSay)
