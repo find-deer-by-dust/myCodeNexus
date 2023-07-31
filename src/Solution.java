d757c01ef8387d7693cbfac526e7ceb73ae7d070
lcyXG <luchengyi@xigua.city>
2023-07-31 17:43:36 +0800

    do nowcoder

--- src/Solution.java
+++ src/Solution.java
@@ -1,40 +1,24 @@
 import java.util.*;
 
-
 public class Solution {
-    /**
-     * 代码中的类名、方法名、参数名已经指定，请勿修改，直接返回方法规定的值即可
-     *
-     * 
-     * @param s string字符串 
-     * @return int整型
-     */
-    public int StrToInt (String s) {
-        // write code here
-        char[] tmp=s.toCharArray();
-        int re=0;
-        boolean findNum=false;
-        boolean negative=false;
-        int i;
-        for(i=0;i<s.length();i++){
-            if(tmp[i]!=' ')
-                break;
-        }
-        if(i==s.length())
-            return re;
-        if(tmp[i]=='+'){
-            i++;
-        }
-           
-        else if(tmp[i]=='-')
-            negative=true;
-        for(;i<s.length();i++){
+    int[][] table;
+    int result = 0;
 
+    public int rectCover(int target) {
+        if (target == 0)
+            return 0;
+        table = new int[target][target];
 
+        return result;
+    }
 
+    void function() {
+        int tmp = 0;
+        for (int[] i : table) {
+            for (int j : i) {
+                tmp = tmp | j;
+            }
         }
-        if(negative)
-            return -re;
-        return re;
+
     }
 }
