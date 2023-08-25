946517e9b244073cc936ead981a0dfd707581eef
lcyXG <luchengyi@xigua.city>
2023-08-25 15:06:54 +0800

    nothing

--- src/Main.java
+++ src/Main.java
@@ -6,17 +6,12 @@ import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
-<<<<<<< HEAD
 		Solution s = new Solution();
 		String a = "ababaaa";
 		String b = "aa";
 		a.indexOf(b);
 		System.out.println(s.kmp(a, b));
 		System.out.println(a.indexOf(b));
-=======
-		Solution s=new Solution();
-		System.out.println();
->>>>>>> 7fe8a3def042d66f9d33bcfd4b347224178a4523
 	}
 
 }
