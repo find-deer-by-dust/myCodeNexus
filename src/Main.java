33ad81792ab28699d653cfbe1a9f2382e2b3e286
find-deer-by-dust <1915141975@qq.com>
2023-09-18 19:35:56 +0800

    做了些笔试

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,34 @@
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Comparator;
 import java.util.Scanner;
 
 public class Main {
-	public static void main(String[] args) {
-		Solution s = new Solution();
+	static int[] list;
+	static int m;
+	static int p = 0;
 
-		System.out.println();
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		m = in.nextInt();
+		int n = in.nextInt();
+		int i;
+		list = new int[n];
+		for (i = 0; i < n; i++) {
+			list[i] = in.nextInt();
+		}
+		function(0, m);
+		System.out.println(p);
 	}
 
+	static void function(int a, int b) {
+		if (b == 0) {
+			p++;
+		}
+		System.out.println(a);
+		System.out.println(b);
+		if (a+1<list.length) {
+			function(a + 1, b - list[a]);
+			function(a + 1, b);
+		}
+	}
 }
