79d41deed15ded9853629c0574c88c867048ece0
find-deer-by-dust <1915141975@qq.com>
2023-09-23 19:04:43 +0800

    做了些笔试

--- src/Main.java
+++ src/Main.java
@@ -1,34 +1,49 @@
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class Main {
+	static int p;
 	static int[] list;
-	static int m;
-	static int p = 0;
+	static int n;
+	static int x;
+	static int y;
 
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
-		m = in.nextInt();
-		int n = in.nextInt();
+		int t = in.nextInt();
 		int i;
-		list = new int[n];
-		for (i = 0; i < n; i++) {
-			list[i] = in.nextInt();
+		for (i = 0; i < t; i++) {
+			p = 0;
+			n = in.nextInt();
+			x = in.nextInt();
+			int j;
+			list = new int[n];
+			for (j = 0; j < n; j++) {
+				list[j] = in.nextInt();
+			}
+			y = n / 2;
+			if (y * 2 != n)
+				y++;
+			function(y, x, 0);
+			System.out.println(p);
 		}
-		function(0, m);
-		System.out.println(p);
+		in.close();
 	}
 
-	static void function(int a, int b) {
-		if (b == 0) {
+	public static void function(int num, int x, int a) {
+		// System.out.println(num + " " + x + " " + a);
+		if (num == 0 && x <= 0) {
+			// System.out.println(p);
 			p++;
-		}
-		System.out.println(a);
-		System.out.println(b);
-		if (a+1<list.length) {
-			function(a + 1, b - list[a]);
-			function(a + 1, b);
+		} 
+		else if (a < list.length) {
+			function(num - 1, x - list[a], a + 1);
+			function(num, x, a + 1);
 		}
 	}
 }
+// 1 5 10 3 2 3 4 5
+// 1 2 5 6 7
--- src/Solution.java
+++ src/Solution.java
@@ -1,40 +1,32 @@
 import java.util.*;
 
-
 public class Solution {
     /**
      * 代码中的类名、方法名、参数名已经指定，请勿修改，直接返回方法规定的值即可
      *
+     * 第N个月的兔子对数总和
      * 
-     * @param s string字符串 
+     * @param monthNum int整型 monthNum个月
      * @return int整型
      */
-    public int StrToInt (String s) {
+    int big = 0;
+    int small1 = 1;
+    int small2 = 0;
+    // int small3=0;
+    // int sum;
+    public int sum_rabbit(int monthNum) {
         // write code here
-        char[] tmp=s.toCharArray();
-        int re=0;
-        boolean findNum=false;
-        boolean negative=false;
-        int i;
-        for(i=0;i<s.length();i++){
-            if(tmp[i]!=' ')
-                break;
+        if (monthNum == 1) {
+            return big+small1+small2;
         }
-        if(i==s.length())
-            return re;
-        if(tmp[i]=='+'){
-            i++;
-        }
-           
-        else if(tmp[i]=='-')
-            negative=true;
-        for(;i<s.length();i++){
-
-
-
-        }
-        if(negative)
-            return -re;
-        return re;
+        int tmp;
+        tmp=small2;
+        // small3=small2;
+        small2=small1;
+        big=big+tmp;
+        small1=big;
+        
+        
+        return sum_rabbit(monthNum - 1);
     }
 }
